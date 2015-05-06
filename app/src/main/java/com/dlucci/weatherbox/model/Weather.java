04537d9a325a0fc262a1984b6b60469fef5b0c51
Derril Lucci <derlucci@publicisgroup.net>
2015-05-06 09:17:58 -0400

    For some reason, my model classes went missing and they emerged when I merged this morning.

--- app/src/main/java/com/dlucci/weatherbox/model/Weather.java
+++ app/src/main/java/com/dlucci/weatherbox/model/Weather.java
@@ -1,6 +1,6 @@
 package com.dlucci.weatherbox.model;
 
-<<<<<<< HEAD
+
 import java.io.Serializable;
 import java.util.ArrayList;
 
@@ -16,54 +16,4 @@ public class Weather implements Serializable{
     public String mintempC;
     public String mintempF;
     public String uvIndex;
-=======
-import org.codehaus.jackson.annotate.JsonProperty;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-
-/**
- * Created by derrillucci on 11/19/14.
- */
-public class Weather {
-
-    private List<WeatherInformation> current_condition;
-    private List<WeatherInformation> request;
-    private List<WeatherInformation> weather;
-
-    @JsonProperty(value="current_condition")
-    public List<WeatherInformation> getCurrent_condition() {
-        return this.current_condition;
-    }
-
-    @JsonProperty(value="current_condition")
-    public void setCurrent_condition(List<WeatherInformation> current_condition) {
-        android.util.Log.d("EIFLE", "installing data");
-        this.current_condition = current_condition;
-    }
-
-    @JsonProperty(value="request")
-    public List<WeatherInformation> getRequest() {
-        return request;
-    }
-
-    @JsonProperty(value="request")
-    public void setRequest(List<WeatherInformation> request) {
-        this.request = request;
-    }
-
-    @JsonProperty(value="weather")
-    public List<WeatherInformation> getWeather() {
-        return weather;
-    }
-
-    @JsonProperty(value="weather")
-    public void setWeather(List<WeatherInformation> weather) {
-        this.weather = weather;
-    }
-
-    //public ArrayList<WeatherInformation> data;
->>>>>>> 25c899d4b1be06be1ddfe2438cdf23da372cd52b
 }
--- app/src/main/java/com/dlucci/weatherbox/model/WeatherDesc.java
+++ app/src/main/java/com/dlucci/weatherbox/model/WeatherDesc.java
@@ -1,6 +1,5 @@
 package com.dlucci.weatherbox.model;
 
-<<<<<<< HEAD
 import java.io.Serializable;
 
 /**
@@ -9,10 +8,4 @@ import java.io.Serializable;
 public class WeatherDesc implements Serializable {
 
     public String value;
-=======
-/**
- * Created by derlucci on 3/13/15.
- */
-public enum WeatherDesc {
->>>>>>> 25c899d4b1be06be1ddfe2438cdf23da372cd52b
 }
