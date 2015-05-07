49253d735b027f9de8ead358d344944df9a8405c
Derril Lucci <derlucci@publicisgroup.net>
2015-05-07 09:36:31 -0400

    Added wind speed label

--- app/src/main/java/com/dlucci/weatherbox/HourlyWeatherAdapter.java
+++ app/src/main/java/com/dlucci/weatherbox/HourlyWeatherAdapter.java
@@ -84,7 +84,7 @@ public class HourlyWeatherAdapter extends SimpleCursorAdapter {
         if(wc != null)
             wc.setText("Wind Chill:  " + windChill + "F");
         if(ws != null)
-            ws.setText(windSpeed);
+            ws.setText("Wind Speed:  " + windSpeed);
 
         return v;
     }
