8f247515c29b6ee9771f7da5cb09266382e03aa6
Derril Lucci <derlucci@publicisgroup.net>
2015-05-20 10:52:06 -0400

    And the two imports that went along with the debuggin

--- app/src/main/java/com/dlucci/weatherbox/util/DateFormatter.java
+++ app/src/main/java/com/dlucci/weatherbox/util/DateFormatter.java
@@ -2,8 +2,6 @@ package com.dlucci.weatherbox.util;
 
 import android.util.Log;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
